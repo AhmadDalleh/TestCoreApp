84434e16c5234b0fa5b1a36626df5c619ed798b5
ahmaddalleh <ahmaddalleh35@gmail.com>
2025-04-02 03:36:25 +0300

    the Css Commit

---
+++ TestCoreApp/Controllers/ItemsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestCoreApp.Data;
+using TestCoreApp.Models;
+
+namespace TestCoreApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public ItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        private readonly ApplicationDbContext _context;
+        public IActionResult Index()
+        {
+            IEnumerable<Item> itemsList = _context.Items.ToList();
+            return View(itemsList);
+        }
+    }
+}
