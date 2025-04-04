c2cc99f5c7cd4715c9cc90dca012b10bbfaa06ca
ahmaddalleh <ahmaddalleh35@gmail.com>
2025-04-04 16:08:22 +0300

    finch add new item

--- TestCoreApp/Controllers/ItemsController.cs
+++ TestCoreApp/Controllers/ItemsController.cs
@@ -16,9 +16,30 @@ namespace TestCoreApp.Controllers
             IEnumerable<Item> itemsList = _context.Items.ToList();
             return View(itemsList);
         }
-        public IActionResult New() {
+        public IActionResult New(){
 
         return View();
         }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult New(Item item)
+        {
+            if (item.Name == "100")
+            {
+                ModelState.AddModelError("Name", "Name can't equal 100");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(item);
+            }
+        }
+
     }
 }
