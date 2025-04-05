907cf26a9937338533d19b646d36923d26918411
ahmaddalleh <ahmaddalleh35@gmail.com>
2025-04-05 04:24:38 +0300

    clint side validation and use data anotation for server side

--- TestCoreApp/Models/Item.cs
+++ TestCoreApp/Models/Item.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TestCoreApp.Models
 {
@@ -11,6 +12,8 @@ namespace TestCoreApp.Models
         public string Name { get; set; }
 
         [Required]
+        [DisplayName("The Price")]
+        [Range(10,100000,ErrorMessage ="Value for {0} must be between {1} and {2}.")]
         public decimal Price { get; set; }
 
         public DateTime CreateDate {  get; set; } = DateTime.Now;
